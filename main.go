e21fa8c9f9ac246a8a4f67649d51d0a310628200
Luther Barnum <luther.barnum@gmail.com>
2018-10-30 21:44:45 -0400

    Added switches and file output instead of stdout

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
-
 
 // https://api.chess.com/pub/player/<player>/games/archives
 type MonthlyArchives struct {
@@ -43,7 +44,14 @@ func main() {
 	var monthlyarchives MonthlyArchives
 	var mygames PlayerGames
 
-	response, err := http.Get("https://api.chess.com/pub/player/lgbarn/games/archives")
+	var CurrPlayer string
+	flag.StringVar(&CurrPlayer, "player", "", "Player to get pgn games")
+	flag.Parse()
+	if CurrPlayer == "" {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+	response, err := http.Get("https://api.chess.com/pub/player/" + CurrPlayer + "/games/archives")
 	if err != nil {
 		fmt.Printf("The HTTP request failed with error %s\n", err)
 	}
@@ -51,6 +59,11 @@ func main() {
 	data, _ := ioutil.ReadAll(response.Body)
 	json.Unmarshal([]byte(data), &monthlyarchives)
 
+	f, err := os.OpenFile(CurrPlayer+".pgn",os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 	for _, archive := range monthlyarchives.Archives {
 		response, err := http.Get(archive)
 		if err != nil {
@@ -60,7 +73,9 @@ func main() {
 		data, _ := ioutil.ReadAll(response.Body)
 		json.Unmarshal(data, &mygames)
 		for _, game := range mygames.Games {
-			fmt.Printf("%s\n\n", string(game.Pgn))
+			if _, err = f.WriteString(game.Pgn); err != nil {
+				panic(err)
+			}
 		}
 	}
 }
