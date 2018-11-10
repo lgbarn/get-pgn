3c2d2e1690886ba707d3e6de7d01858ee0dbdc47
Luther Barnum <luther.barnum@gmail.com>
2018-11-10 17:59:10 -0500

    Added additional options to give user option for single file or multiple files

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 // https://api.chess.com/pub/player/<player>/games/archives
@@ -17,13 +18,18 @@ type MonthlyArchives struct {
 func main() {
 	var monthlyarchives MonthlyArchives
 	var CurrPlayer string
+	var UseSingleFile bool
 
-	flag.StringVar(&CurrPlayer, "player", "", "Player to get pgn games")
+	flag.StringVar(&CurrPlayer, "p", "", "Player to get pgn games")
+	//flag.BoolVar(&UseMonthlyFiles, "monthly", false, "Save to monthly files")
+	flag.BoolVar(&UseSingleFile, "s", false, "Save to a single file")
 	flag.Parse()
 	if CurrPlayer == "" {
 		flag.PrintDefaults()
 		os.Exit(0)
 	}
+
+	var currFile = CurrPlayer + ".pgn"
 	response, err := http.Get("https://api.chess.com/pub/player/" + CurrPlayer + "/games/archives")
 	if err != nil {
 		fmt.Printf("The HTTP request failed with error %s\n", err)
@@ -31,19 +37,31 @@ func main() {
 	defer response.Body.Close()
 	data, _ := ioutil.ReadAll(response.Body)
 	json.Unmarshal([]byte(data), &monthlyarchives)
-	f, err := os.OpenFile(CurrPlayer+".pgn", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
+
 	for _, archive := range monthlyarchives.Archives {
+
+		splitArchive := strings.Split(string(archive), "/")
+		year := splitArchive[7]
+		month := splitArchive[8]
+		fmt.Printf("Downloading files from %s/%s for %s\n", month, year, CurrPlayer)
+
+		if !UseSingleFile {
+			currFile = (CurrPlayer + "_" + year + "-" + month + ".pgn")
+		}
+		fmt.Println(currFile)
+		f, err := os.OpenFile(currFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
 		response, err := http.Get(archive + "/pgn")
 		if err != nil {
 			fmt.Printf("The HTTP request failed with error %s\n", err)
 		}
 		defer response.Body.Close()
 		data, _ := ioutil.ReadAll(response.Body)
-		if _, err = f.WriteString(string(data) + "\n"); err != nil {
+		_, err = f.WriteString(string(data) + "\n")
+		if err != nil {
 			panic(err)
 		}
 	}
