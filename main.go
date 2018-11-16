33018ab1abb5032023d0aee844716cb4d1688f69
Luther Barnum <luther.barnum@gmail.com>
2018-11-16 13:53:49 -0500

    Change the way the -l option is used to get last X number of months

--- main.go
+++ main.go
@@ -10,19 +10,58 @@ import (
 	"strings"
 )
 
-// https://api.chess.com/pub/player/<player>/games/archives
-type MonthlyArchives struct {
+// monthlyArchives https://api.chess.com/pub/player/<player>/games/archives
+type monthlyArchives struct {
 	Archives []string `json:"archives"`
 }
 
+// getArchiveList returns a list of achive links
+func (MonthlyArchives *monthlyArchives) getArchiveList() []string {
+	var archiveList []string
+	for _, archive := range MonthlyArchives.Archives {
+		archiveList = append(archiveList, archive)
+	}
+	return archiveList
+}
+
+// reverseArchiveList returns archive links in reverse order
+func (MonthlyArchives *monthlyArchives) reverseArchiveList() []string {
+	var archiveList []string
+	for _, archive := range MonthlyArchives.Archives {
+		archiveList = append(archiveList, archive)
+	}
+	for i := len(archiveList)/2 - 1; i >= 0; i-- {
+		opp := len(archiveList) - 1 - i
+		archiveList[i], archiveList[opp] = archiveList[opp], archiveList[i]
+	}
+	return archiveList
+}
+
+// getLastXof get X number months
+func getLastXof(number int, inList []string) []string {
+	var outList []string
+	for i := len(inList)/2 - 1; i >= 0; i-- {
+		opp := len(inList) - 1 - i
+		inList[i], inList[opp] = inList[opp], inList[i]
+	}
+	for i := 0; i < number; i++ {
+		outList = append(outList, inList[i])
+	}
+	for i := len(outList)/2 - 1; i >= 0; i-- {
+		opp := len(outList) - 1 - i
+		outList[i], outList[opp] = outList[opp], outList[i]
+	}
+	return outList
+}
+
 func main() {
-	var monthlyarchives MonthlyArchives
+	var monthlyarchives monthlyArchives
 	var CurrPlayer string
 	var UseSingleFile bool
-	var getLastMonth bool
+	var getLastMonth int
 
 	flag.StringVar(&CurrPlayer, "p", "", "Player to get pgn games")
-	flag.BoolVar(&getLastMonth, "l", false, "Get last month of pgn games")
+	flag.IntVar(&getLastMonth, "l", 0, "Get last month of pgn games")
 	flag.BoolVar(&UseSingleFile, "s", false, "Save to a single file")
 	flag.Parse()
 	if CurrPlayer == "" {
@@ -39,10 +78,12 @@ func main() {
 	data, _ := ioutil.ReadAll(response.Body)
 	json.Unmarshal([]byte(data), &monthlyarchives)
 
-	if getLastMonth {
-		newArchive := monthlyarchives.Archives[len(monthlyarchives.Archives)-1]
-		monthlyarchives.Archives = nil
-		monthlyarchives.Archives = append(monthlyarchives.Archives, newArchive[:])
+	if getLastMonth > 0 {
+		if getLastMonth > len(monthlyarchives.Archives) {
+			getLastMonth = len(monthlyarchives.Archives)
+			fmt.Printf("Only %d Number of months are available, returning %d number of months instead\n", getLastMonth, getLastMonth)
+		}
+		monthlyarchives.Archives = getLastXof(getLastMonth, monthlyarchives.getArchiveList())
 	}
 
 	for _, archive := range monthlyarchives.Archives {
